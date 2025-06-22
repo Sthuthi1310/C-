20fb2ad6cce55533505dd7f12a09dc1e3496d596
Sthuthi <sthuthisheela@gmail.com>
2025-06-22 09:58:08 +0530

    sum of 2 nums

---
+++ sum.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter the value of a and b: "<<endl;
+    cin>>a>>b;
+    c=a+b;
+    cout<<"Sum of "<<a<<" and "<<b<<" is "<<c<<endl;
+}
