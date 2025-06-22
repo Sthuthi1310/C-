bdf81c05b76f6f0226a915e49023efc0aee5cbaa
Sthuthi <sthuthisheela@gmail.com>
2025-06-22 10:02:12 +0530

    square

---
+++ square.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,p;
+    cout<<"Enter the value of a "<<endl;
+    cin>>a;
+    p=a*a;
+    cout<<"Square of "<<a<<" is "<<p<<endl;
+    
+}
